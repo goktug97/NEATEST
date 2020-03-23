8f8c1f5e662988ce3b4e763009e633b47e50e8e3
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-03-23 19:18:11 +0300

    gym cartpole

---
+++ cartpole.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import gym
+import matplotlib.pyplot as plt
+
+import neat
+
+cartpole_ai = neat.NEAT(n_networks = 1000,
+                        input_size = 4,
+                        output_size = 1,
+                        bias = True,
+                        c1 = 1.0, c2 = 1.0, c3 = 3.0,
+                        distance_threshold = 4.0,
+                        weight_mutation_rate = 0.8,
+                        node_mutation_rate = 0.03,
+                        connection_mutation_rate = 0.3,
+                        interspecies_mating_rate = 0.001,
+                        disable_rate = 0.75,
+                        stegnant_threshold = 15,
+                        input_activation = neat.steepened_sigmoid,
+                        hidden_activation = neat.steepened_sigmoid,
+                        output_activation = neat.steepened_sigmoid)
+
+env = gym.make('CartPole-v0')
+max_fitness = -float('inf')
+while True:
+    for genome in cartpole_ai.population:
+        obsvervation = env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            output  = int(round(genome(obsvervation)[0]))
+            observation, reward, done, info = env.step(output)
+            total_reward += reward
+        genome.fitness = total_reward
+        if total_reward > max_fitness:
+            max_fitness = total_reward
+            best_genome = genome
+    print(f'Generation: {cartpole_ai.generation}, Max Fitness: {max_fitness}')
+    plt.cla()
+    best_genome.draw()
+    plt.draw()
+    plt.pause(0.001)
+    cartpole_ai.next_generation()
+env.close()
+
