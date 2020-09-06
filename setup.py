62de9bdbd2adbf45f915202e3b68ab5dc9d652bd
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-09-06 13:11:04 +0300

    renaming

--- setup.py
+++ setup.py
@@ -7,18 +7,16 @@ directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='NEAT',
-      version='1.0.1',
-      description='Python Implementation of NEAT Genetic Algorithm',
+setup(name='NEATEST',
+      version='1.0.0',
+      description='Evolving Neural Networks through Augmenting Topologies with'
+      'Evolution Strategy Training',
       author='Göktuğ Karakaşlı',
       author_email='karakasligk@gmail.com',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      url='https://github.com/goktug97/NEAT',
-      download_url=(
-          'https://github.com/goktug97/NEAT/archive/v1.0.1.tar.gz'),
-      packages = ['neat'],
+      packages = ['neatest'],
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: MIT License",
